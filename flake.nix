c8d4285b2ed0ee1afdba00a1f5fd5f3ed57b9e88
Edward Hesketh <inbox@edwardh.dev>
2026-09-23 17:27:11 +0100

    feat: tidy flake

--- flake.nix
+++ flake.nix
@@ -2,19 +2,10 @@
   description = "RP2040 PIO emulator frontend";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/26.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     flake-utils.url = "github:numtide/flake-utils";
-    gomod2nix = {
-      url = "github:nix-community/gomod2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
-    gotk4-nix = {
-      url = "github:diamondburned/gotk4-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.gomod2nix.follows = "gomod2nix";
-    };
+    gomod2nix.url = "github:nix-community/gomod2nix";
+    gotk4-nix.url = "github:diamondburned/gotk4-nix";
   };
 
   outputs =
@@ -24,32 +15,30 @@
       flake-utils,
       gomod2nix,
       gotk4-nix,
-      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         pname = "pio-gtk";
+
         pio-gtk = (pkgs.extend gomod2nix.overlays.default).buildGoApplication {
           inherit pname;
           version = self.shortRev or self.dirtyShortRev or "dev";
           src = ./.;
           modules = ./gomod2nix.toml;
+
           nativeBuildInputs = with pkgs; [
             pkg-config
+            gobject-introspection
             wrapGAppsHook4
-            git
           ];
+
           buildInputs = with pkgs; [
             gtk4
             libadwaita
-            glib
-            gdk-pixbuf
-            librsvg
-            gobject-introspection
-            hicolor-icon-theme
           ];
+
           doCheck = false;
         };
 
@@ -62,27 +51,12 @@
         };
       in
       {
-        packages = {
-          inherit pio-gtk;
-          default = pio-gtk;
-        };
-
-        apps.default = {
-          type = "app";
-          program = "${pio-gtk}/bin/${pname}";
-        };
+        packages.default = pio-gtk;
 
         devShells.default = gotk4.mkShell {
           buildInputs = [
             gomod2nix.packages.${system}.default
-            pkgs.libadwaita
           ];
-          clangdPackages =
-            pkgs: with pkgs; [
-              gtk4
-              glib
-              libadwaita
-            ];
         };
       }
     );
